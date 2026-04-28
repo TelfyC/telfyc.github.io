1c247fe7594d3cfea69620c8f21ea48eb82267b2
juntao.ji@formal-tech.com <juntao.ji@formal-tech.com>
2026-04-28 13:03:56 +0800

    init: configure

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Your Name";
-export const AUTHOR_NAME = "Your Name";
-export const AUTHOR_INITIAL = "YN";
-export const SITE_DESCRIPTION = "Your Name's personal website.";
+export const SITE_TITLE = "telfyc";
+export const AUTHOR_NAME = "telfyc";
+export const AUTHOR_INITIAL = "T";
+export const SITE_DESCRIPTION = "telfyc's personal website.";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
 
